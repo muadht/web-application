1147593c01e1ccc7057c3f6bd9a9bc856a945447
Muadh Taghdi <muadht@gmail.com>
2024-11-11 21:22:43 -0500

    static root

--- simpleSignage/simpleSignage/settings.py
+++ simpleSignage/simpleSignage/settings.py
@@ -122,7 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Default primary key field type
